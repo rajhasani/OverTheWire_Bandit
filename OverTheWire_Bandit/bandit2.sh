a4f8a993fde41b4bf7d59ab1d1255eeaad3f0a7d
Raveesh Raj Hasani <114200807+rajhasani@users.noreply.github.com>
2022-12-27 12:42:19 -0600

    Create bandit2.sh

---
+++ OverTheWire_Bandit/bandit2.sh
@@ -0,0 +1,6 @@
+## The password is located in a file named "-" in the home directory. Since the filename is a symbol rather than a conventional character, we will use the ./ notation to reference the relative path to the current working directory
+
+bandit1@bandit:~$ ls
+-
+bandit1@bandit:~$ cat ./-
+rRGizSaX8Mk1RTb1CNQoXTcYZWU6lgzi
