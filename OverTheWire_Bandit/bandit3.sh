70792867ea37c7136b8ee41a895be4ecd8deac3a
Raveesh Raj Hasani <114200807+rajhasani@users.noreply.github.com>
2022-12-27 12:51:12 -0600

    Create bandit3.sh

---
+++ OverTheWire_Bandit/bandit3.sh
@@ -0,0 +1,10 @@
+## The password is located in a file that has spaces in the filename. Therefore, each word in the filename with a space after it will need to be appended with a backslash (\)
+
+bandit2@bandit:~$ ls
+spaces in this filename
+bandit2@bandit:~$ cat spaces\ in\ this\ filename
+aBZ0W5EmUfAf7kHTQeOwd8bauFJ2lAiG
+bandit2@bandit:~$ ssh bandit3@localhost -p 2220
+bandit3@localhost's password: aBZ0W5EmUfAf7kHTQeOwd8bauFJ2lAiG
+
+## It is worth noting you can use "Tab" on your keyboard to auto-complete file references within the command line. In this instance, you can press Tab after typing in "spaces" to autocomplete the rest of the filename, backslashes included. This technique will be used in later levels to our advantage.
